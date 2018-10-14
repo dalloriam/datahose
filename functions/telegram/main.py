85f993d9292837ffd897c8765a967fde084e74c3
Dalloriam <dalloriam@gmail.com>
2018-10-13 21:26:19 -0400

    Ditch twx

--- functions/telegram/main.py
+++ functions/telegram/main.py
@@ -1,12 +1,26 @@
-from twx.botapi import TelegramBot
-
 import base64
 import json
+import requests
 import os
 
 
 def _format(notification: dict) -> str:
     return f'*{notification.get("sender", "Unknown")}* - {notification["message"]}'
+
+
+def send_message(body: dict):
+    bot_key = os.environ.get('BOT_KEY')
+    conv_id = os.environ.get('CONVERSATION_ID')
+
+    formatted = _format(body)
+    requests.post(
+        url=f'https://api.telegram.org/bot{bot_key}/sendMessage',
+        json={
+            'chat_id': conv_id,
+            'text': formatted,
+            'parse_mode': 'Markdown'
+        }
+    )
 
 
 def telegram_send(event, context):
@@ -15,11 +29,6 @@ def telegram_send(event, context):
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
-    bot_key = os.environ.get('BOT_KEY')
-    conv_id = os.environ.get('CONVERSATION_ID')
-    bot = TelegramBot(bot_key)
-
     evt = json.loads(base64.b64decode(event['data']).decode('utf-8'))
-    notification = evt['body']
-
-    bot.send_message(conv_id, _format(notification), parse_mode='Markdown')
+    send_message(evt['body'])
+    print(f'Sent message to [{os.environ.get("CONVERSATION_ID")}]')
