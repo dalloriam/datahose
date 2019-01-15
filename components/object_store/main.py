9b1bd162834b7879107068b1247878bff12aa4cc
Dalloriam <dalloriam@gmail.com>
2019-01-14 23:29:44 -0500

    Fix object store

--- components/object_store/main.py
+++ components/object_store/main.py
@@ -6,13 +6,27 @@ import os
 import zlib
 
 
+def fetch_config() -> dict:
+    bucket_name = os.environ.get('CONFIG_BUCKET_NAME')
+    storage_client = storage.Client()
+
+    bucket = storage_client.get_bucket(bucket_name)
+    config_blob = bucket.get_blob('services/datahose.json')
+    config = json.loads(config_blob.download_as_string())
+
+    return config
+
+
+config = fetch_config()
+
+
 def obj_consume(event, context):
     """Triggered from a message on a Cloud Pub/Sub topic.
     Args:
          event (dict): Event payload.
          context (google.cloud.components.Context): Metadata for the event.
     """
-    bucket_name = os.environ.get('BUCKET_NAME')
+    bucket_name = config['buckets']['archives']
     client = error_reporting.Client()
 
     try:
